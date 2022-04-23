addecbb4d1fba43a7cb47f033029d02f0426502d
IAMMORBOR <ofamotibe@gmail.com>
2022-04-23 06:49:43 +0100

    compleeted potfolio

---
+++ blog/script.js
@@ -0,0 +1,20 @@
+document.querySelector('#blog').style.color = 'red';
+
+function openMenu(){
+    var menuPage = document.querySelector ('#mobile_navBar').style.display = 'none';
+}
+document.querySelector('#hambugerMenu').addEventListener ('click', function(){
+    if(openMenu){
+        var menuPage = document.querySelector ('#mobile_navBar').style.display = 'block';
+    }
+    
+})
+function closeMenu(){
+    var crossPage = document.querySelector ('#crossMenu').style.display = 'block';
+}
+document.querySelector('#crossMenu').addEventListener ('click', function(){
+    if(closeMenu){
+        var menuPage = document.querySelector ('#mobile_navBar').style.display = 'none';
+    }
+    
+})
